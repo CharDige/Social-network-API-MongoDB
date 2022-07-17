11f876f3117ac6650e8451d771a5b5dbdce6e148
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-17 20:33:32 +0930

    Added thoughtSchema, created reactionCount virtual for total reactions, and created Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -30,3 +30,41 @@ const reactionSchema = new Schema(
         id: false,
     }
 );
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtValue) => moment(createdAtValue).format('DD MMMM YYYY, h:mm a'),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Virtual to get total amount of reactions in one thought
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
