79eadbdc2350ffcd2727e703a4cc6432fbe82dfa
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-17 22:27:55 +0930

    Added functionality to get one thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,4 +7,16 @@ module.exports = {
             .then((thoughts) => res.json(thoughts))
             .catch((err) => res.status(500).json(err));
     },
+    // Get one thought
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .populate('reactions')
+        .select('-__v')
+        .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: 'No thought with this ID' })
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    }
 }
