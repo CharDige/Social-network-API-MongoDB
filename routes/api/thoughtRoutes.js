a660ba55726850725a88645ba9c532e89fdc4015
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-18 21:07:19 +0930

    Added thought controller functions and the GET, POST, PUT and DELETE requests

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(removeThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
