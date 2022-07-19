a58ff34f9cd024e6c2ea9897a478dd97b0ff08ed
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-19 21:02:08 +0930

    Removed .populate as causing bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,14 +13,12 @@ module.exports = {
     // Get one user
     getOneUser(req, res) {
         User.findOne({ _id: req.params.userId })
-        .populate('thoughts')
-        .populate('friends')
         .select('-__v')
-        .then((userData) => 
-            !userData
+        .then((user) =>
+            !user
                 ? res.status(404).json({ message: 'No user with that ID' })
-                : res.json(userData)
-        ) 
+                : res.json(user)
+        )
         .catch((err) => res.status(500).json(err));
     },
     // Create a user
